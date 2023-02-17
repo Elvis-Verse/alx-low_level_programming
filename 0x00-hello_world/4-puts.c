e27886160c3592dbd4e29295564a49ec696c3541
Miracle <miracleelvis2017@gmail.com>
2023-02-16 18:47:36 -0800

    my first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
+/*
  * main - print a line with puts function
- * Return: 0 (Success)
+ * Return: 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
 }
 
